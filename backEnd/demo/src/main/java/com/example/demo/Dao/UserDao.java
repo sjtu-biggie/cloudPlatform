3b80d781f7dd2caece2f7a78669018f08dea3e49
ynjgzfly <1921209391@qq.com>
2020-09-15 23:53:01 +0800

    删除了第一次写的后端登陆

--- backEnd/demo/src/main/java/com/example/demo/Dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Dao;
-
-import com.example.demo.Entity.User;
-
-public interface UserDao {
-    User getUserByName(String username);
-}
--- backEnd/demo/src/main/java/com/example/demo/Repository/JpaRepositoriy.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.Repository;
-
-public interface JpaRepositoriy {
-
-}

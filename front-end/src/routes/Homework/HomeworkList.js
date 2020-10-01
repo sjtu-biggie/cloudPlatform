d7de4c3ace79109e5903747f3ca432037ebdfe86
LeBronQ <2279489119@qq.com>
2020-10-01 10:34:28 +0800

    Update HomeworkList.js

--- front-end/src/routes/Homework/HomeworkList.js
+++ front-end/src/routes/Homework/HomeworkList.js
@@ -53,7 +53,8 @@ class HomeworkList extends React.Component {
                               renderItem={item=>{
                                   return (
                                       <List.Item
-                                          actions={[<IconText type="file-text" text="100" />, <IconText type="calendar" text="截止：2020-10-1 20:00" />, <IconText type="schedule" text="已提交" />, <IconText type="clock-circle-o" text="已结束" />,<Button type="danger">删除</Button>]}
+                                          actions={[<IconText type="file-text" text="100" />, <IconText type="calendar" text="截止：2020-10-1 20:00" />, <IconText type="schedule" text="已提交" />, <IconText type="clock-circle-o" text="已结束" />]}
+                                          extra={(this.state.delete === false ? []:[<Button type="danger">删除</Button>])}
                                           >
                                           <List.Item.Meta
                                               avatar={<Avatar src={item.avatar} />}

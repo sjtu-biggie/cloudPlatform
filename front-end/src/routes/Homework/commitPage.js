d1ddb463b16be7bf27b5a2db15035a457738cebd
Passion-Love <1481042653@qq.com>
2020-10-28 12:04:00 +0800

    Update commitPage.js

--- front-end/src/routes/Homework/commitPage.js
+++ front-end/src/routes/Homework/commitPage.js
@@ -11,7 +11,6 @@ const { Text, Link } = Typography;
 class commitPage extends React.Component{
 
     state={
-        avatar: '../../assets/img/mistakes.png',
         content: '已知：如图，P是正方形ABCD内点，∠PAD=∠PDA=15° 求证：△PBC是正三角形',
         endTime:"2020-10-1 20:00",
         editorState: EditorState.createEmpty(),
@@ -82,9 +81,6 @@ class commitPage extends React.Component{
                     <Row>
                         <Col span={20}>{this.state.content}</Col>
                         <Col span={4}>{this.state.overDdl===true?"已截止 |":"未截止 |"}{this.state.isCommit===true?" 已提交":" 未提交"}</Col>
-                    </Row>
-                    <Row>
-                        <img  alt="logo" src={require("../../assets/img/mistakes.png" )}/>
                     </Row>
                     <Row>
                         <Text type={"secondary"}>{"截止日期："+this.state.endTime}</Text>

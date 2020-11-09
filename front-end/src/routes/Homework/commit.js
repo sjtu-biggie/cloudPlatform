eecb94c1e10f710853a1f33e35ba094707956a85
sjtu-biggie <zhull991122@sjtu.edu.cn>
2020-11-09 16:03:09 +0800

    feat:syllabus

--- front-end/src/routes/Homework/commit.js
+++ front-end/src/routes/Homework/commit.js
@@ -16,6 +16,17 @@ const { Text, Link } = Typography;
 class HomeworkCommit extends React.Component{
 
     state={
+        syllabus: {
+            chapterNum: 1,
+            chapter1: {
+                title: "题目",
+                type:'选择题',
+                text:'请输入题干，若是填空题请显式标记出要填的空来，并和填空题的答案一一对应',
+                content: [
+
+                ]
+            }
+        },
         homeworkId:"",
         content:"",
         comment:"",
@@ -100,7 +111,7 @@ class HomeworkCommit extends React.Component{
             .catch(function (error) {
                 console.log(error);
             });
-    }
+    };
 
     changeData=(e)=>{//input输入什么，就监听这个方法，然后再修改state，然后返回到视图
         this.setState({

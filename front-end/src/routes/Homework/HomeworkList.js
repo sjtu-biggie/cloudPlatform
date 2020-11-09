dd95098aff541aff6d856e3c4d2b670d7cd5d33f
sjtu-biggie <zhull991122@sjtu.edu.cn>
2020-11-09 20:47:21 +0800

    feat:syllabus

--- front-end/src/routes/Homework/HomeworkList.js
+++ front-end/src/routes/Homework/HomeworkList.js
@@ -222,7 +222,7 @@ class HomeworkList extends React.Component {
                                               fontSize:'18px',
                                               marginTop: '10px',
                                               height: '20px'
-                                          }}>{item.type==="主观题"||item.content.chapter1===undefined?item.content:item.content.chapter1.text}</p>}
+                                          }}>{item.type==="主观题"||item.syllabus===undefined?item.content:item.syllabus.chapter1.text}</p>}
                                       </List.Item>
                                   )
                               }}

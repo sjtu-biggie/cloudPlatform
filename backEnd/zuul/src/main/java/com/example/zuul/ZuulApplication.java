8d13763591603362beb907bb3f34f664c982b723
sjtu-biggie <zhull991122@sjtu.edu.cn>
2020-11-16 19:27:18 +0800

    fix:application.yml

--- backEnd/zuul/src/main/java/com/example/zuul/ZuulApplication.java
+++ backEnd/zuul/src/main/java/com/example/zuul/ZuulApplication.java
@@ -2,8 +2,9 @@ package com.example.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@
+@EnableZuulProxy
 @SpringBootApplication
 public class ZuulApplication {
 

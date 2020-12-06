1cbe416c8771094ee5b27c33b22dfa7222905b54
sjtu-biggie <zhull991122@sjtu.edu.cn>
2020-12-06 17:23:46 +0800

    feat:帮助文档优化

--- front-end/src/routes/Display/ListDemo/index.js
+++ front-end/src/routes/Display/ListDemo/index.js
@@ -61,64 +61,132 @@ class ListDemo extends React.Component {
           <Button style={!loadingMore?{}:{display:'none'}} onClick={() => this.getData2()}>加载更多</Button>
       </div>
     );
-    return (
-      <Card>
-        <CustomBreadcrumb arr={['帮助']}/>
-          <div  className='card-item'  style={{minHeight:70}}>
-            <p   style={{position:"absolute",fontSize:70,left:"25%",fontFamily:"黑体"}}>
-              学易-云作业平台 ® 帮助文档
-            </p>
-          </div>
-        <Row gutter={10}>
-          <Col span={12}  >
-            <Card     cover={
-              <img
-                  height={350}
-                  alt="example"
-                  src={require("../../../pic/courseset.jpg")}
-              />
-            } bordered={false} className='card-item' style={{height:350}} >
-              <p style={{position:'absolute',left:"35%",top:"50%",fontSize:'70px',fontWeight:'bold',color:"white"}} onClick={()=>{this.setState({helpType:1})}}> 课程创建 </p>
-            </Card>
-          </Col>
-          <Col span={12}>
-            <Card     cover={
-              <img
-                  height={350}
-                  alt="example"
-                  src={require("../../../pic/coursemanage.jpg")}
-              />
-            } bordered={false} className='card-item' style={{height:350}} >
-              <p style={{position:'absolute',left:"35%",top:"50%",fontSize:'70px',fontWeight:'bold',color:"white"}}onClick={()=>{this.setState({helpType:2})}}> 课程管理 </p>
-            </Card>
-          </Col>
-        </Row>
-        <Row gutter={10}>
-          <Col span={12}  >
-            <Card     cover={
-              <img
-                  height={350}
-                  alt="example"
-                  src={require("../../../pic/homeworkassign.jpg")}
-              />
-            } bordered={false} className='card-item' style={{height:350}} >
-              <p style={{position:'absolute',left:"35%",top:"50%",fontSize:'70px',fontWeight:'bold',color:"white"}} onClick={()=>{this.setState({helpType:3})}}> 作业布置 </p>
-            </Card>
-          </Col>
-          <Col span={12}>
-            <Card     cover={
-              <img
-                  height={350}
-                  alt="example"
-                  src={require("../../../pic/gradeBackground.jpg")}
-              />
-            } bordered={false} className='card-item' style={{height:350}} >
-              <p style={{position:'absolute',left:"35%",top:"50%",fontSize:'70px',fontWeight:'bold',color:"white"}} onClick={()=>{this.setState({helpType:4})}}> 作业批改 </p>
-            </Card>
-          </Col>
-        </Row>
-      </Card>
-    )
+    switch(this.state.helpType){
+      case 0:
+        if(this.state.role==='teacher'){
+          return (
+              <Card>
+                <CustomBreadcrumb arr={['帮助']}/>
+                <div  className='card-item'  style={{minHeight:70}}>
+                  <p   style={{position:"absolute",fontSize:70,left:"25%",fontFamily:"黑体"}}>
+                    学易-云作业平台 ® 帮助文档
+                  </p>
+                </div>
+                <Row gutter={10}>
+                  <Col span={12}  >
+                    <Card     cover={
+                      <img
+                          height={350}
+                          alt="example"
+                          src={require("../../../pic/courseset.png")}
+                      />
+                    } bordered={false} className='card-item' style={{height:350}} >
+                      <p style={{position:'absolute',left:"35%",top:"50%",fontSize:'70px',fontWeight:'bold',color:"black"}} onClick={()=>{this.setState({helpType:1})}}> 课程创建 </p>
+                    </Card>
+                  </Col>
+                  <Col span={12}>
+                    <Card     cover={
+                      <img
+                          height={350}
+                          alt="example"
+                          src={require("../../../pic/coursemanage.png")}
+                      />
+                    } bordered={false} className='card-item' style={{height:350}} >
+                      <p style={{position:'absolute',left:"35%",top:"50%",fontSize:'70px',fontWeight:'bold',color:"black"}}onClick={()=>{this.setState({helpType:2})}}> 课程管理 </p>
+                    </Card>
+                  </Col>
+                </Row>
+                <Row gutter={10}>
+                  <Col span={12}  >
+                    <Card     cover={
+                      <img
+                          height={350}
+                          alt="example"
+                          src={require("../../../pic/homeworkassign.png")}
+                      />
+                    } bordered={false} className='card-item' style={{height:350}} >
+                      <p style={{position:'absolute',left:"35%",top:"50%",fontSize:'70px',fontWeight:'bold',color:"black"}} onClick={()=>{this.setState({helpType:3})}}> 作业布置 </p>
+                    </Card>
+                  </Col>
+                  <Col span={12}>
+                    <Card     cover={
+                      <img
+                          height={350}
+                          alt="example"
+                          src={require("../../../pic/gradeBackground.png")}
+                      />
+                    } bordered={false} className='card-item' style={{height:350}} >
+                      <p style={{position:'absolute',left:"35%",top:"50%",fontSize:'70px',fontWeight:'bold',color:"black"}} onClick={()=>{this.setState({helpType:4})}}> 作业批改 </p>
+                    </Card>
+                  </Col>
+                </Row>
+              </Card>
+          )
+        }
+        break;
+      case 1:
+        if(this.state.role==='teacher'){
+          return (
+              <Card style={{fontSize:20}}>
+                <CustomBreadcrumb arr={['帮助']}/>
+                <h1 style={{fontWeight:'bold'}}>创建课程</h1>
+                <p>
+                  教师在开启课堂之前需要先创建一门课程。<br/>
+                  点击“所有课程”进入课程页面，点击“创建一门新的课程”。
+                </p>
+                <img
+                    height={500}
+                    alt="example"
+                    src={require("../../../pic/c1.jpg")}
+                />
+                <br/>
+                <p>
+                  ①第一环节，界面显示课程的基本信息表单，包括课程名称、目标年级、课程类型（语文/数学/英语...）、课程教材、详细介绍、课程起止时间（一般为一个学期时间）、上课班级。<br/>
+                  带星号的都是必填项，你需要输入或选择对应栏的内容，随后点击“提交”进入下一环节。
+                </p>
+                <img
+                    height={500}
+                    alt="example"
+                    src={require("../../../pic/c2.png")}
+                />
+                <br/>
+                <p>
+                  ②第二环节确定课程的基本大纲。<br/>
+                  一般来说课程大纲分为大的章节和下面的小节，点击“在此添加章节”创建章节，在每个章节栏目下面的“添加一个小节”创建小节。<br/>
+                  需要修改大纲时，点击红色按钮“删除这个章节”/“删除这个小节”进行对应位置的章节/小节的删除（注意，删除章节时会连带下方的小节一同删除，请谨慎操作！）。
+                </p>
+                <img
+                    height={500}
+                    alt="example"
+                    src={require("../../../pic/c3.jpg")}
+                />
+                <p>
+                  ③第三环节，添加这门课程的学生。<br/>
+                  该节目左侧是课程学生名单，右侧是你在第一环节输入的上课班级的学生名单。<br/>
+                  在右侧名单中勾选对应学生栏目，点击左上方“添加到上课表”按钮即可把该学生添加到左边课程名单中，对于已添加的学生同样可以点击栏目右侧的“删除”按钮将操作撤回。<br/>
+                  需要在比较长的名单中找到某个学生的栏目时，在名单上方的搜索框中输入学生名字/学号并点击“搜索”进行查找。
+                </p>
+                <img
+                    height={500}
+                    alt="example"
+                    src={require("../../../pic/c4.jpg")}
+                />
+                <p>
+                  三个环节全部完成，点击下方“确认”按钮完成课程创建
+                </p>
+              </Card>
+
+          )
+        }
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+    }
+
   }
 }
 

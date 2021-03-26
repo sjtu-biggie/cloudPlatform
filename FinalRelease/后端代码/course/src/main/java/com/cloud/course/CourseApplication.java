ffa223494265e2af66d3f304f6802c86d67ad07b
sjtu-biggie <zhull991122@sjtu.edu.cn>
2021-03-26 22:18:10 +0800

    software testing

--- FinalRelease/后端代码/course/src/main/java/com/cloud/course/CourseApplication.java
+++ FinalRelease/后端代码/course/src/main/java/com/cloud/course/CourseApplication.java
@@ -2,9 +2,7 @@ package com.cloud.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
 public class CourseApplication {
 

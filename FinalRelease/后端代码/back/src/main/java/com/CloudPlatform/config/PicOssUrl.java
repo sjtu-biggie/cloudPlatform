1d1aadf96fb321eef9b666847e3f130b8a721b55
ygen <52997248+sjtu-biggie@users.noreply.github.com>
2021-07-05 14:36:43 +0800

    Update PicOssUrl.java

--- FinalRelease/后端代码/back/src/main/java/com/CloudPlatform/config/PicOssUrl.java
+++ FinalRelease/后端代码/back/src/main/java/com/CloudPlatform/config/PicOssUrl.java
@@ -11,8 +11,8 @@ public class PicOssUrl {
         testUploadFile(file);
     }
     public static String testUploadFile(String file) throws ClientException, IOException, com.aliyuncs.exceptions.ClientException {
-        String accessKey = "LTAI4G6u4Vad9GaYfLjy3ncD";
-        String accessKeySecret = "g96N07QKeFydCSbXsFcgRbdsSex5Rk";
+        String accessKey = "";
+        String accessKeySecret = "";
         FileUtils fileUtils = FileUtils.getInstance(accessKey,accessKeySecret);
         //String file = "https://fuss10.elemecdn.com/5/32/c17416d77817f2507d7fbdf15ef22jpeg.jpeg";
         String ossurl = fileUtils.upload(file);
